ce5af7b7217164d9b9be05c49bd0a4e499e11490
harshitha <harshitha@gmail.com>
2024-06-12 10:20:27 +0530

    first commit

---
+++ run.py
@@ -0,0 +1,19 @@
+from Core.data import Data
+from Core.config import Column
+from Core.model import LSTM_Trainer
+
+data = Data()
+data.read('Data/TSLA (1).csv')
+data.check_null_values()
+data.clean_data()
+print(Column.OPEN.value)
+data.print_head()
+data.print_description()
+data.normalize()
+data.visualize(Column.OPEN.value)
+data.visualize(Column.CLOSE.value)
+
+trainer = LSTM_Trainer(data.dataframe, data.scaler)
+trainer.build_and_train_lstm()
+trainer.predict_and_plot()
+trainer.evaluate_model()
